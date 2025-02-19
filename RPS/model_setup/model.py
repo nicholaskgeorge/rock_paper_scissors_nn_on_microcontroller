9d15b769eaf08b996c268ae491d0463b1868a676
Nicholas George <nkg37@cornell.edu>
2025-02-18 20:32:25 -0500

    removed pooling layers so that converts to tmdl

--- RPS/model_setup/model.py
+++ RPS/model_setup/model.py
@@ -1,22 +1,19 @@
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, Dropout, Input
 
 def create_model():
     model = Sequential([
         # Conv layer 1
         Input(shape=(96, 96, 1)),
-        Conv2D(32, (3, 3), activation='relu'),  # Use batch_input_shape,  # Single channel
-        MaxPooling2D(2, 2),
+        Conv2D(32, (3, 3), activation='relu', strides=(2, 2)),  # Stride of 2 reduces spatial dimensions
         
         # Conv layer 2
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D(2, 2),
+        Conv2D(64, (3, 3), activation='relu', strides=(2, 2)),
         
         # Conv layer 3
-        Conv2D(128, (3, 3), activation='relu'),
-        MaxPooling2D(2, 2),
+        Conv2D(128, (3, 3), activation='relu', strides=(2, 2)),
         
-        # Flatten and dense layer
+        # Flatten and dense layers
         Flatten(),
         Dense(256, activation='relu'),
         Dropout(0.5),
