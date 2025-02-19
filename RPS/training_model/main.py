0fee48cfd4a55470471c9523b74a36b3fa27cce3
Nicholas George <nkg37@cornell.edu>
2025-02-19 10:01:41 -0500

    shrank model down to about 659 KB

--- RPS/training_model/main.py
+++ RPS/training_model/main.py
@@ -6,7 +6,7 @@ rps_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 # Add "RPS" to sys.path
 sys.path.append(rps_path)
 
-from model_setup.model import create_model
+from model_setup.model2 import create_model
 from model_setup.utils import load_data
 
 from tensorflow_model_optimization.python.core.keras.compat import keras
@@ -23,19 +23,19 @@ model = create_model()
 
 # Define EarlyStopping callback
 early_stopping = keras.callbacks.EarlyStopping(
-    monitor='accuracy',    # Metric to monitor
+    monitor='val_accuracy',    # Metric to monitor
     min_delta=0.01,       # Minimum change to qualify as improvement
     patience=3,            # Number of epochs with no improvement after which training will be stopped
     verbose=1
 )
 
-EPOCHS = 4
+EPOCHS = 40
 history = model.fit(
     train_data,
     validation_data=val_data,
     epochs=EPOCHS,
-    verbose = 1,
-    callbacks=[early_stopping] # List of callbacks
+    verbose = 1
+    #callbacks=[early_stopping] # List of callbacks
 )
 
 # Save the trained model
